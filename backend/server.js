d1b1021a0b335b969cc6c67c1c3cf7b84f5ecbc8
D-K-01 <animelegens3@gmail.com>
2025-10-26 14:30:58 +0200

    checks and imports
    server check and express + dotenv imports

--- backend/server.js
+++ backend/server.js
@@ -1,2 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+
+app.get("/", (req, res) => {
+    res.send("it works ig");
+})
+
+app.listen(5000, () => {
+    console.log("Server started at http://localhost:5000")
+});
